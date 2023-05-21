6d774fa0a7228201b5c1edbf4762cbf0d1de053e
Suraj mandage <69792855+SurajM556@users.noreply.github.com>
2023-05-21 16:47:24 +0530

    first commit

---
+++ src/app/employe-details/employe-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employe-details',
+  templateUrl: './employe-details.component.html',
+  styleUrls: ['./employe-details.component.css'],
+})
+export class EmployeDetailsComponent implements OnInit {
+  id!: number;
+  employee!: Employee;
+
+  constructor(
+    private route: ActivatedRoute,
+    private employeeService: EmployeeService
+  ) {}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeeService.getEmployeeById(this.id).subscribe((data) => {
+      this.employee = data;
+    });
+  }
+}
